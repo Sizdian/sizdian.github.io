b8d405831c3760d0032f7721cd8e4eee2f3ab47d
Sizdian <jtwieb@myseneca.ca>
2015-11-27 19:16:22 -0500

    test

---
+++ util/header.php
@@ -0,0 +1,89 @@
+<html>
+	<head>
+		<style>
+			h2 {
+				font-family: Arial, Helvetica, sans-serif;
+			}
+
+			h3 {
+				font-family: Arial, Helvetica, sans-serif;
+			}
+			
+			th {
+				font-family: Arial, Helvetica, sans-serif;
+			}
+		</style>
+
+		<script src="http://popcornjs.org/code/dist/popcorn-complete.js"></script>
+		<script src="processing.js"></script>
+
+
+		<title>$pageTitle</title>
+	</head>
+
+	<body bgcolor="B8561D">
+
+		<div style="border:solid; display: inline-block">
+			<table onclick="window.document.location='index.php';" onmouseover="this.style.backgroundColor = '#A34C1A'" onmouseout="this.style.backgroundColor = ''">
+				<tr>
+					<td><img src="logo.gif"></td>
+					<td><h1 style="font-family: 'Arial Black', Gadget, sans-serif">BTC640</h1></td>
+				</tr>
+			</table>
+		</div>
+
+		<table style="background-color:black; width:100%">
+			<tr>
+				<th style="background-color:#A00000" onmouseover="this.style.backgroundColor = '#FF0000'" onmouseout="this.style.backgroundColor = '#A00000'" onclick="window.document.location='a1.php';">
+					Assignment 1
+				</th>
+
+				<th style="background-color:#A00000" onmouseover="this.style.backgroundColor = '#FF0000'" onmouseout="this.style.backgroundColor = '#A00000'" onclick="window.document.location='a2.php';">
+					Assignment 2
+				</th>
+
+				<th style="background-color:#A00000" onmouseover="this.style.backgroundColor = '#FF0000'" onmouseout="this.style.backgroundColor = '#A00000'" onclick="window.document.location='a3.php';">
+					Assignment 3
+				</th>
+			</tr>
+		</table>
+
+		<table style="background-color:black; width:100%">
+			<tr>
+				<th style="background-color:#0000A0" onmouseover="this.style.backgroundColor = '#0000FF'" onmouseout="this.style.backgroundColor = '#0000A0'" onclick="window.document.location='lab2.php';">
+					Lab 2
+				</th>
+
+				<th style="background-color:#0000A0" onmouseover="this.style.backgroundColor = '#0000FF'" onmouseout="this.style.backgroundColor = '#0000A0'" onclick="window.document.location='lab3.php';">
+					Lab 3
+				</th>
+
+				<th style="background-color:#808080" onmouseover="this.style.backgroundColor = '#9C9C9C'" onmouseout="this.style.backgroundColor = '#808080'" onclick="window.document.location='lab4.php';">
+					Lab 4
+				</th>
+
+				<th style="background-color:#808080" onmouseover="this.style.backgroundColor = '#9C9C9C'" onmouseout="this.style.backgroundColor = '#808080'" onclick="window.document.location='lab5.php';">
+					Lab 5
+				</th>
+
+				<th style="background-color:#0000A0" onmouseover="this.style.backgroundColor = '#0000FF'" onmouseout="this.style.backgroundColor = '#0000A0'" onclick="window.document.location='lab6.php';">
+					Lab 6
+				</th>
+
+				<th style="background-color:#0000A0" onmouseover="this.style.backgroundColor = '#0000FF'" onmouseout="this.style.backgroundColor = '#0000A0'" onclick="window.document.location='lab7.php';">
+					Lab 7
+				</th>
+
+				<th style="background-color:#0000A0" onmouseover="this.style.backgroundColor = '#0000FF'" onmouseout="this.style.backgroundColor = '#0000A0'" onclick="window.document.location='lab8.php';">
+					Lab 8
+				</th>
+
+				<th style="background-color:#0000A0" onmouseover="this.style.backgroundColor = '#0000FF'" onmouseout="this.style.backgroundColor = '#0000A0'" onclick="window.document.location='lab9.php';">
+					Lab 9
+				</th>
+
+				<th style="background-color:#0000A0" onmouseover="this.style.backgroundColor = '#0000FF'" onmouseout="this.style.backgroundColor = '#0000A0'" onclick="window.document.location='lab10.php';">
+					Lab 10
+				</th>
+			</tr>
+		</table>
